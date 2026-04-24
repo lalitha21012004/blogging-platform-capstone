a30b759952731c8445b2a8ce5dd94cb0e30645a0
lalitha21012004 <lochlalitha67@gmail.com>
2026-04-24 20:39:36 +0530

    Fix:correctly included server files

---
+++ server/models/Posts.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose');
+
+const postSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:false
+    },
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    categories:{
+        type:Array,
+        required:false
+    }
+},{ timestamps:true});
+
+module.exports=mongoose.model('Post',postSchema);
